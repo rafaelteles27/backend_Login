2b9ec46572719eb99787c7023c6e9fc92972e45b
rafaelteles27 <rteles27@gmx.com>
2023-05-23 22:39:02 +0100

    added another functional login and registration backend

---
+++ src/main/java/net/guides/springboot/registrationlogindemo/repository/UserRepository.java
@@ -0,0 +1,8 @@
+package net.guides.springboot.registrationlogindemo.repository;
+
+import net.guides.springboot.registrationlogindemo.entity.User;
+import org.springframework.data.jpa.repository.JpaRepository;
+
+public interface UserRepository extends JpaRepository<User, Long> {
+    User findByEmail(String email);
+}
